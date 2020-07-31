836b4a1ee8379824cc1f04405902db2f7ac7499d
Rachel Plesha <rplesha@stsci.edu>
2020-07-31 16:12:15 -0400

    Playing around with plotly

--- ScienceSaturday02/beer_plots.py
+++ ScienceSaturday02/beer_plots.py
@@ -28,53 +28,55 @@ df_ratings = pd.read_csv(os.path.join(data_dir, 'SS2_beerScores.csv'))
 
 ratings_only = df_ratings.drop(columns='Beer_Letter')
 
-# fig = px.scatter(ratings_only, y=ratings_only.mean(axis=1), size=ratings_only.std(axis=1), error_y=ratings_only.std(axis=1), color=ratings_only.mean(axis=1))
-# fig.show()
+fig = px.scatter(df_ratings, y=df_ratings.mean(axis=1), size=df_ratings.std(axis=1), error_y=df_ratings.std(axis=1))#, #color=ratings_only.mean(axis=1))
 
-beer_order = []
-beer = []
-beer_ratings = []
-people = []
-for beer_letter in df_order['Beer_Letter']:
-    for person in ratings_only.keys():
-        try:
-            wh_beer = np.where(np.array(df_order[person]) == beer_letter)[0][0]
-        except IndexError:
-            continue
-        beer_ratings.append(ratings_only[person][wh_beer])
-        beer_order.append(wh_beer)
-        #print(beer_letter, wh_beer, df_order['Beer'][df_order['Beer_Letter'] == beer_letter].iloc[0])
-        beer.append(df_order['Beer'][df_order['Beer_Letter'] == beer_letter].iloc[0])
-        people.append(person)
-df = pd.DataFrame({'beer' : beer, 'order' : beer_order, 'rating' : beer_ratings, 'person' : people})
-
-#fig = px.scatter(df, x='order', y='rating', color='beer')
-
-df = df.sort_values(by=['order'])
-
-fig = go.Figure()
-for beer in np.unique(df['beer']):
-    mask = df['beer'] == beer
-    fig.add_trace(go.Scatter(x=df['order'][mask],
-                             y=df['rating'][mask],
-                             mode='markers',
-                             marker=dict(size=12,
-                                         color=['red']),
-                             name = beer
-                                    ))
-
-    p = find_fit(df['order'][mask], df['rating'][mask])
-
-    if p != None:
-        fig.add_trace(go.Scatter(x=df['order'][mask],
-                                 y=p(df['order'][mask]),
-                                 mode='lines',
-                                 name=f'{beer} Fit',
-                                 line = dict(width=4,
-                                             dash='dash')
-                                ))
-
+#fig = px.scatter(ratings_only, y=ratings_only.mean(axis=1), size=ratings_only.std(axis=1), error_y=ratings_only.std(axis=1))#, #color=ratings_only.mean(axis=1))
 fig.show()
+
+# beer_order = []
+# beer = []
+# beer_ratings = []
+# people = []
+# for beer_letter in df_order['Beer_Letter']:
+#     for person in ratings_only.keys():
+#         try:
+#             wh_beer = np.where(np.array(df_order[person]) == beer_letter)[0][0]
+#         except IndexError:
+#             continue
+#         beer_ratings.append(ratings_only[person][wh_beer])
+#         beer_order.append(wh_beer)
+#         #print(beer_letter, wh_beer, df_order['Beer'][df_order['Beer_Letter'] == beer_letter].iloc[0])
+#         beer.append(df_order['Beer'][df_order['Beer_Letter'] == beer_letter].iloc[0])
+#         people.append(person)
+# df = pd.DataFrame({'beer' : beer, 'order' : beer_order, 'rating' : beer_ratings, 'person' : people})
+#
+# #fig = px.scatter(df, x='order', y='rating', color='beer')
+#
+# df = df.sort_values(by=['order'])
+#
+# fig = go.Figure()
+# for beer in np.unique(df['beer']):
+#     mask = df['beer'] == beer
+#     fig.add_trace(go.Scatter(x=df['order'][mask],
+#                              y=df['rating'][mask],
+#                              mode='markers',
+#                              marker=dict(size=12,
+#                                          color=['red']),
+#                              name = beer
+#                                     ))
+#
+#     p = find_fit(df['order'][mask], df['rating'][mask])
+#
+#     if p != None:
+#         fig.add_trace(go.Scatter(x=df['order'][mask],
+#                                  y=p(df['order'][mask]),
+#                                  mode='lines',
+#                                  name=f'{beer} Fit',
+#                                  line = dict(width=4,
+#                                              dash='dash')
+#                                 ))
+#
+# fig.show()
 
 # fig = go.Figure()
 # for person in ratings_only.keys():
